1db832fd57790ff178ad77f53944d9cbed17624f
Seweryn <sewcio0212@gmail.com>
2024-04-29 08:53:37 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react"; // Dodanie importu React
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <div>
+        <button className="span-two">AC</button>
+        <button>DEL</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button> {/* Poprawiony przycisk 8 */}
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className="span-two">=</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
